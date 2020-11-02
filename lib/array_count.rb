ad4198ac87559df0a1e03cefae42f5bb42e0ddaf
Winston Chen <winstonchen.wm@gmail.com>
2020-11-02 21:52:41 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,9 +6,9 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  count = 0 
-  array.count do |x|
-    x.class == String 
+  array.count {|element|
+    element == ""  
+  }
   end
   count
 end
